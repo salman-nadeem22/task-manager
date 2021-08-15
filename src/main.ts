d1210650c104c4d92c00c3b9085b07c03255ce4b
Salman Nadeem <65226911+salman-nadeem22@users.noreply.github.com>
2021-08-15 17:44:52 +0500

    Did some fixes

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const logger = new Logger();
 
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   const port = 3000;
